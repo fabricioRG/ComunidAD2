0a46ef03e1ba067078825d7f16ef588a1e07da91
fabricioRG <fabricio21rg@gmail.com>
2021-02-21 14:26:57 -0600

    implementing material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,74 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { DataService } from './data.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './views/profile/profile.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { DashboardUsersComponent } from './views/dashboard-users/dashboard-users.component';
+import { HeaderComponent } from './views/header/header.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+const rutas: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'inicio'
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      // {
+      //   path: '',
+      //   pathMatch: 'prefix',
+      //   redirectTo: 'users'
+      // },
+      {
+        path: 'users',
+        component: DashboardUsersComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    DashboardComponent,
+    HeaderComponent,
+    InicioComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(rutas, {
+      useHash: true
+    }),
   ],
-  providers: [],
+  exports: [
+  ],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    registroAcademico: number | undefined;
+    nombreCompleto: string | undefined;
+    password: string | undefined;
+    fechaDeNacimiento: string | undefined;
+    genero: string | undefined;
+    fotoDePerfil: string | undefined;
+    correoElectronico: string | undefined;
+    rolUsuario: string | undefined;
+    ciudad: string | undefined;
+    estado: string | undefined;
+
+}
