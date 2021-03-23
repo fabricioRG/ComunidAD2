998a7c2ddb186aeeacd25b97e3941bd01d20a216
fabricioRG <fabricio21rg@gmail.com>
2021-03-22 23:43:45 -0600

    problems fixed

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -6,6 +6,7 @@ import { User } from 'src/app/user.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ActiveModalComponent } from 'src/app/components/active-modal/active-modal.component';
 import { ModalService } from 'src/app/services/modal/modal.service';
+import { SesionService } from 'src/app/services/sesion/sesion.service';
 
 
 @Component({
@@ -19,11 +20,12 @@ export class ProfileComponent implements OnInit {
   usuario: User = new User();
   banderaEstadoActivo = true;
 
-  constructor(private dataService: DataService,public validacionURL: LoginService, private constantesService : ConstantesService,private _modalService: NgbModal,private _modalPropio: ModalService) {
+  constructor(private dataService: DataService, public validacionURL: LoginService, private constantesService: ConstantesService, private _modalService: NgbModal, private _modalPropio: ModalService, public sessionService: SesionService) {
     this.token = localStorage.getItem('token');
-  if(this.token!=null){
-  this.token = JSON.parse(this.token).token
-  } }
+    if (this.token != null) {
+      this.token = JSON.parse(this.token).token
+    }
+  }
 
   ngOnInit(): void {
     var aux = new User();
@@ -35,7 +37,7 @@ export class ProfileComponent implements OnInit {
         this.usuario = user;
         console.log("se obtuvo: ")
         console.log(this.usuario)
-        if(user.estado=="INACTIVO"){
+        if (user.estado == "INACTIVO") {
           this.banderaEstadoActivo = false
         }
       },
@@ -46,15 +48,15 @@ export class ProfileComponent implements OnInit {
   }
 
 
-  mensajeEstadoBoton(): String{
-    if(this.usuario.estado == ConstantesService.ESTADO_USUARIO_ACTIVO){
+  mensajeEstadoBoton(): String {
+    if (this.usuario.estado == ConstantesService.ESTADO_USUARIO_ACTIVO) {
       return ConstantesService.ESTADO_USUARIO_INACTIVO
-    }else{
-      return  ConstantesService.ESTADO_USUARIO_ACTIVO
+    } else {
+      return ConstantesService.ESTADO_USUARIO_ACTIVO
     }
   }
 
-  cambiarEstado(){
+  cambiarEstado() {
     const modal = this._modalService.open(ActiveModalComponent);
     modal.componentInstance.modalHeader = "CONFIRMACION DE CAMBIO";
     modal.componentInstance.modalBodyTitle = "A";
@@ -66,45 +68,45 @@ export class ProfileComponent implements OnInit {
       this.dataService.updateUser(this.usuario).subscribe(
         (user) => {
           this.mostrarMensaje('USUARIO ' + this.usuario.nombreCompleto + ' ACTUALIZADO CON EXITO')
-          
+
         },
         (error) => {
           this.usuario.estado = this.revertirEstado()
-          this.mostrarError(error.error,1)
+          this.mostrarError(error.error, 1)
         }
       );
     }, (reason) => {
     });
   }
 
-  mostrarError(mensajesError: any, contadorErrores: number): void{
-    this._modalPropio.openModal("ERROR EN CAMBIO DE ESTADO DE USUARIO",mensajesError + '\n TOTAL DE ERRORES: ' + contadorErrores,"NO APROBADO",false)
-    
+  mostrarError(mensajesError: any, contadorErrores: number): void {
+    this._modalPropio.openModal("ERROR EN CAMBIO DE ESTADO DE USUARIO", mensajesError + '\n TOTAL DE ERRORES: ' + contadorErrores, "NO APROBADO", false)
+
   }
 
-  mostrarMensaje(mensaje: any):void{
+  mostrarMensaje(mensaje: any): void {
     console.log("entre modal")
-    this._modalPropio.openModal("CAMBIO DE ESTADO DE USUARIO CORRECTO",mensaje,"APROBADO",false)
+    this._modalPropio.openModal("CAMBIO DE ESTADO DE USUARIO CORRECTO", mensaje, "APROBADO", false)
   }
 
- 
 
-  obtenerEstadoString(): string{
-    if(this.banderaEstadoActivo){
+
+  obtenerEstadoString(): string {
+    if (this.banderaEstadoActivo) {
       this.banderaEstadoActivo = !this.banderaEstadoActivo
       return ConstantesService.ESTADO_USUARIO_INACTIVO
-    }else{
+    } else {
       this.banderaEstadoActivo = !this.banderaEstadoActivo
       return ConstantesService.ESTADO_USUARIO_ACTIVO
     }
   }
 
 
-  revertirEstado():string{
-    if(this.banderaEstadoActivo){
+  revertirEstado(): string {
+    if (this.banderaEstadoActivo) {
       this.banderaEstadoActivo = !this.banderaEstadoActivo
       return ConstantesService.ESTADO_USUARIO_ACTIVO
-    }else{
+    } else {
       this.banderaEstadoActivo = !this.banderaEstadoActivo
       return ConstantesService.ESTADO_USUARIO_INACTIVO
     }
