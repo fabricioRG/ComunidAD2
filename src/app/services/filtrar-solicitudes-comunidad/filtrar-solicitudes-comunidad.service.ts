03c3c2634c71880dc9ef97ed04ee016162cfdfc0
[Juan Pablo Orizabal] <juanpablo-orizabalgil@cunoc.edu.gt>
2021-03-31 22:27:22 -0600

    Funcionalidad completada

--- src/app/services/filtrar-solicitudes-comunidad/filtrar-solicitudes-comunidad.service.ts
+++ src/app/services/filtrar-solicitudes-comunidad/filtrar-solicitudes-comunidad.service.ts
@@ -2,21 +2,43 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeadersService } from '../headers/headers.service';
 import { ComunityAssign } from 'src/app/models/comunityAssign.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FiltrarSolicitudesComunidadService {
+  filtrarSolicitudesComunidadURL =
+    '/api/comunity/filtrarSolicitudesComunidades';
+  actualizarEstadoSolicitudesComunidadURL =
+    '/api/users/updateStateComunityRequest';
+  deleteComunityURL = '/api/comunity/deleteComunity';
 
-  filtrarSolicitudesComunidadURL = '/api/comunity/filtrarSolicitudesComunidades';
-  actualizarEstadoSolicitudesComunidadURL = '/api/users/updateStateComunityRequest';
+  constructor(
+    private _http: HttpClient,
+    private controllHeader: HeadersService
+  ) {}
 
-  constructor(private _http: HttpClient,private controllHeader: HeadersService) { }
-
-  getAsignacionesComunidad(token: any,filtros: any){
-    return this._http.post<ComunityAssign[]>(this.filtrarSolicitudesComunidadURL,filtros,this.controllHeader.obtenerHeaderConToken(token));
+  getAsignacionesComunidad(token: any, filtros: any) {
+    return this._http.post<ComunityAssign[]>(
+      this.filtrarSolicitudesComunidadURL,
+      filtros,
+      this.controllHeader.obtenerHeaderConToken(token)
+    );
   }
-  actualizarEstadoAsignacionesComunidad(token: any,datos: ComunityAssign){
-    return this._http.post<ComunityAssign>(this.actualizarEstadoSolicitudesComunidadURL,datos,this.controllHeader.obtenerHeaderConToken(token));
+  actualizarEstadoAsignacionesComunidad(token: any, datos: ComunityAssign) {
+    return this._http.post<ComunityAssign>(
+      this.actualizarEstadoSolicitudesComunidadURL,
+      datos,
+      this.controllHeader.obtenerHeaderConToken(token)
+    );
+  }
+
+  deleteComunity(token: any, filtros: any): Observable<any> {
+    return this._http.post<any>(
+      this.deleteComunityURL,
+      filtros,
+      this.controllHeader.obtenerHeaderConToken(token)
+    );
   }
 }
