da04534a317a98e43faa18f5359abaf5907e2d37
[Juan Pablo Orizabal] <juanpablo-orizabalgil@cunoc.edu.gt>
2021-04-06 02:19:21 -0600

    tests

--- src/app/services/inicializacion-usuario/inicializacion-usuario.service.spec.ts
+++ src/app/services/inicializacion-usuario/inicializacion-usuario.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { User } from 'src/app/user.model';
 import { FechasService } from '../fechas/fechas.service';
-
 import { InicializacionUsuarioService } from './inicializacion-usuario.service';
+import values from 'src/app/Test/ArchivosJson/User2.json';
 
 describe('InicializacionUsuarioService', () => {
   let service: InicializacionUsuarioService;
@@ -14,17 +14,32 @@ describe('InicializacionUsuarioService', () => {
 
   it('crearUsuario ver si asigna correctamente genero', () => {
     //arrange
-    var usuario =  new User();
-    
-    usuario.fechaDeNacimiento = '2000-01-01'
-    usuario.genero = 'M'
-    var fechasService = TestBed.inject(FechasService)
-    spyOn(fechasService, "convertirFecha").and.returnValue('2000-01-01');
+    var usuario = new User();
+
+    usuario.fechaDeNacimiento = '2000-01-01';
+    usuario.genero = 'M';
+    var fechasService = TestBed.inject(FechasService);
+    spyOn(fechasService, 'convertirFecha').and.returnValue('2000-01-01');
     //act
-    service.setFechasService(fechasService)
-    var resultado = service.inicializarValoresUsuario(usuario)
+    service.setFechasService(fechasService);
+    var resultado = service.inicializarValoresUsuario(usuario);
 
     //assert
-    expect(resultado.genero).toEqual(usuario.genero)
+    expect(resultado.genero).toEqual(usuario.genero);
+  });
+  it('crearUsuario ver si asigna correctamente genero', () => {
+    //arrange
+    var usuario = new User();
+
+    usuario.fechaDeNacimiento = '2000-01-01';
+    usuario.genero = 'M';
+    var fechasService = TestBed.inject(FechasService);
+    spyOn(fechasService, 'convertirFecha').and.returnValue('2000-01-01');
+    //act
+    service.setFechasService(fechasService);
+    var resultado = service.asignarCamposEdicionUsuario(usuario, values);
+
+    //assert
+    expect(resultado.ciudad).toEqual(usuario.ciudad);
   });
 });
--- src/app/services/uploadFileService/upload-file-service.service.spec.ts
+++ src/app/services/uploadFileService/upload-file-service.service.spec.ts
@@ -4,7 +4,8 @@ import {
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { User } from 'src/app/user.model';
-
+import user from 'src/app/Test/ArchivosJson/User3.json';
+import arregloAsigns from 'src/app/Test/ArchivosJson/AsignacionesComunidad.json';
 import { UploadFileServiceService } from './upload-file-service.service';
 
 describe('UploadFileServiceService', () => {
@@ -32,5 +33,25 @@ describe('UploadFileServiceService', () => {
       `${service.uploadImageCreateComunityURL}`
     );
     expect(request.request.method).toBe('POST');
+  });
+
+  it('upload profile user image', () => {
+    service.uploadProfileUserImage(null, user).subscribe((result: User) => {
+      expect(result).toEqual(user);
+    });
+
+    const request = httpMock.expectOne(`${service.uploadUserImageURL}`);
+    expect(request.request.method).toBe('POST');
+    request.flush(user);
+  });
+  it('upload comunity post image', () => {
+    service.uploadCommunityPostImage(null, user).subscribe((result: User) => {
+      expect(result).toEqual(arregloAsigns[0]);
+    });
+    const request = httpMock.expectOne(
+      `${service.uploadCommunityPostImageURL}`
+    );
+    expect(request.request.method).toBe('POST');
+    request.flush(arregloAsigns[0]);
   });
 });
