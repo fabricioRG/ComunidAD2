71d3918deeccec2a3bab53223561277e9a3ef60a
JesfrinGnZ <jess2298gnz@gmail.com>
2021-04-11 21:40:26 -0600

    Se puede agregar comentarios

--- src/app/comunity/view-comunity/view-comunity.component.ts
+++ src/app/comunity/view-comunity/view-comunity.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ActivatedRouteSnapshot, Router, ParamMap } from '@angular/router';
 import { DataService } from 'src/app/data.service';
 import { OrdinaryObject } from 'src/app/helpers/ordinary-object.model';
+import { CommentPost } from 'src/app/models/commentPost.model';
 import { Comunity } from 'src/app/models/comunity.model';
 import { ComunityAssign } from 'src/app/models/comunityAssign.model';
 import { CommunityPost } from 'src/app/models/comunityPost.model';
 import { Course } from 'src/app/models/course.model';
 import { IdComunityAssign } from 'src/app/models/idComunityAssign.model';
+import { CommentService } from 'src/app/services/comment/comment.service';
 import { FiltrarSolicitudesComunidadService } from 'src/app/services/filtrar-solicitudes-comunidad/filtrar-solicitudes-comunidad.service';
 import { ModalService } from 'src/app/services/modal/modal.service';
 import { SesionService } from 'src/app/services/sesion/sesion.service';
@@ -27,7 +29,8 @@ const defaultPicture = "";
 export class ViewComunityComponent implements OnInit {
 
   constructor(private redirection: Router, private route: ActivatedRoute, private uploadFileService: UploadFileServiceService,
-    private dataService: DataService, private sessionService: SesionService, private formBuilder: FormBuilder, private modal: ModalService, private comunidadService: FiltrarSolicitudesComunidadService) {
+    private dataService: DataService, private sessionService: SesionService, private formBuilder: FormBuilder, private modal: ModalService, private comunidadService: FiltrarSolicitudesComunidadService,
+    private commentService: CommentService) {
     this.cargarComunidad();
   }
 
@@ -54,6 +57,9 @@ export class ViewComunityComponent implements OnInit {
   alertClosedSuccess = false;
   alertClosedDanger = false;
   disableCreateCommunityPost = false;
+  disableCreateComment = false;
+
+
 
   ngOnInit(): void {
     this.newCommunityPost = new CommunityPost();
@@ -73,6 +79,25 @@ export class ViewComunityComponent implements OnInit {
     })
   }
 
+  //Elementos para crear un comentario
+
+
+  saveComment(post: CommunityPost) {
+    if (post.nuevoComentario && post.nuevoComentario.length != 0 && post.id
+      && this.user.registroAcademico) {
+      var commentPost: CommentPost = this.commentService.generateCommentPost(post.nuevoComentario, this.user.registroAcademico, post.id);
+      var user : User = this.sessionService.getUserWithToken();
+      this.commentService.createComment(commentPost,user)
+      .subscribe((data)=>{
+        data.user=this.user
+        console.log("Comentario:",data)
+        post.nuevoComentario=""
+        post.commentPost?.push(data)
+      })
+    }
+  }
+
+
 
   cargarComunidad() {
     this.comunityAssign = new ComunityAssign();
@@ -80,7 +105,7 @@ export class ViewComunityComponent implements OnInit {
     this.user = new User();
     var idComunidad: string | null = this.route.snapshot.paramMap.get('id');
     //var idComunidad="20"
-    console.log("JAXD",this.route.snapshot.paramMap.get('id'))
+    console.log("JAXD", this.route.snapshot.paramMap.get('id'))
     console.log('ID COMUNIDAD ESCOGIDA', idComunidad);
     //Ver si hay una sesion, de no haber sesion mandarlo al inicio
     //Si hay sesion buscar el usuario
@@ -96,10 +121,10 @@ export class ViewComunityComponent implements OnInit {
       console.log("VOY A REDIRIGIR")
       this.redirection.navigate(['inicio']);
     }
-    
+
   }
 
-  verificarOpcionesParaComunidad() : boolean{
+  verificarOpcionesParaComunidad(): boolean {
     this.dataService.getUserByToken(this.sessionService.getUserWithToken()).subscribe(response => {
       this.user = response;
       console.log("USUARIO get token:", this.user)
@@ -110,8 +135,8 @@ export class ViewComunityComponent implements OnInit {
         if (response.comunity) {
           this.comunity = response.comunity;
         }
-        console.log("JAXDD",this.comunityAssign.user?.registroAcademico)
-        console.log("JAXDDD",this.user.registroAcademico)
+        console.log("JAXDD", this.comunityAssign.user?.registroAcademico)
+        console.log("JAXDDD", this.user.registroAcademico)
         this.loadImageCommunity();
         this.getAllCommunityPost();
         this.getAllUsersInCommunity();
@@ -273,7 +298,7 @@ export class ViewComunityComponent implements OnInit {
   }
 
   loadImageCommunity() {
-    console.log("LOAD",this.comunity)
+    console.log("LOAD", this.comunity)
     if (this.comunity.datosFoto) {
       this.styleBackgroundImageCommunity = encabezadoFoto + this.comunity.datosFoto + finalFoto;
     } else {
@@ -287,6 +312,12 @@ export class ViewComunityComponent implements OnInit {
     }
     this.dataService.getAllCommunityPostByCommunity(search, this.user)
       .subscribe(data => {
+        console.log("POSTS:", data)
+        /*        if(data){
+                  if(data[0].commentPost){
+                    data[0].commentPost[0].descripcion
+                  }
+                }*/
         this.communityPostList = data;
       });
   }
